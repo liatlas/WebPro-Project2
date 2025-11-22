423070863c53a07ecf5b55286d904f58670404b5
liatlas <atlasli2016@gmail.com>
2025-11-22 07:42:57 -0500

    adds banker_offer function

--- logic/banker_logic.php
+++ logic/banker_logic.php
@@ -1,8 +1,17 @@
 <?php
 function banker_offer($remaining_values, $round) {
+    // output is calculated by taking the average
+    // reducing it according to the volatility(spread)
+    // increasing it by the round
     $avg = array_sum($remaining_values) / count($remaining_values);
 
     $max = max($remaining_values);
     $min = min($remaining_values);
+    $volatilty = ($max - $min) / $max;
+
+    $round_factor = 0.35 + (round * 0.08);
+
+    $offer = $avg * (1 - $volatilty) * round_factor;
+    return round($offer, 2);
 }
 ?>
